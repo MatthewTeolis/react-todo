78846362da065e4636c9f2d4b8cfb0474c6d2f12
Matthew Teolis <matthewteolis@gmail.com>
2020-03-10 22:11:46 -0400

    Implemented register and login endpoints

---
+++ backend/app/controllers/authentication_controller.rb
@@ -0,0 +1,51 @@
+class AuthenticationController < ApplicationController
+
+  def register
+    unless validate_register_model
+      render json: { error: "invalid params" }
+      return
+    end
+
+    @user = User.new(register_params)
+
+    if @user.save
+      render json: { status: 201, message: "successfully created user", token: JwtHelper.encode(user_id: @user.id) }, status: :created
+    else
+      render json: { error: "failed to create user." }, status: :internal_server_error
+    end
+  end
+
+  def login
+    unless validate_login_model
+      render json: { error: "invalid params" }
+      return
+    end
+
+    @user = User.find_by_email(login_params[:email])
+
+    if @user&.authenticate(login_params[:password])
+      token = JwtHelper.encode(user_id: @user.id)
+      render json: { status: 200, message: nil, token: token }, status: :ok
+    else
+      render json: { status: 401, message: "unauthorized", token: nil }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def validate_register_model
+    !(params[:email].nil? or params[:firstName].nil? or params[:lastName].nil? or params[:password].nil?)
+  end
+
+  def register_params
+    params.permit(:email, :firstName, :lastName, :password)
+  end
+
+  def validate_login_model
+    !(params[:email].nil? or params[:password].nil?)
+  end
+
+  def login_params
+    params.permit(:email, :password)
+  end
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :firstName, presence: true
+  validates :lastName, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
